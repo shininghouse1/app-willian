c9f32873074e6a488ab46389e02614a78015f46e
André Luferat <Andr Luferat>
2019-09-18 08:25:25 -0300

    Comentários e ajustes menores

--- js/index.js
+++ js/index.js
@@ -1,62 +1,53 @@
 /* SCRIPTS DO TEMA */
 
-console.log('oi')
-
+/* Constructor - Comente ou remova as linhas 'console.log' quando o App estiver pronto! */
 var app = {
-    // inicialização do jQuery e Cordova
+    // Inicialização do jQuery e Cordova (em breve)
     start: function(){
+        console.log('1) Inicializando App...')
         $(document).ready(this.config);
     },
 
-    // configura o app (firebase ou local)
+    // Configura o app (firebase e/ou local)
     config: function(){
-        console.log('configurando o app');
+        console.log('2) Configurando App...');
         app.run();
     },
 
-    // executa o app que está na função 'runApp()'
+    // Executa o app que está na função 'runApp()'
     run: function(){
-        console.log("executando o app");
+        console.log('3) Executando o App...');
         runApp();
     }
 }
 
+/* 'Control' do menu principal */
 function toggleMenu(){
-    if( $('nav').attr('class') == 'slideOn' ){
-        $('nav').attr('class', function(){
-            $('#menuModal').fadeOut('fast');
-
-            // Efeito do botão de menu
-            $('#menu').removeClass('rotateMenuBtn'); // usando elipsis
-            // $('#menu').html('<i class="fas fa-fw fa-bars"></i>'); // usando bars
-
-            return 'slideOff';
+    if( $('nav').attr('class') == 'slideOn' ){ // Se o menu está aparecendo...
+        $('nav').attr('class', function(){ // Altera a classe do menu
+            $('#menuModal').fadeOut('fast'); // Esconde fundo do menu com fade
+            $('#menu').removeClass('rotateMenuBtn'); // Remove classe que rotaciona o botão
+            return 'slideOff'; // Aplica classe que desloca o menu para a esquerda, escondendo
         });
-    } else {
-        $('nav').attr('class', function(){
-            $('#menuModal').fadeIn('fast');
-            
-            // Efeito do botão de menu
-            $('#menu').addClass('rotateMenuBtn'); // usando elipsis
-            // $('#menu').html('<i class="fas fa-fw fa-times"></i>'); // usando bars
-            
-            return 'slideOn';
+    } else { // Se o menu está oculto...
+        $('nav').attr('class', function(){ // Altera a classe do menu
+            $('#menuModal').fadeIn('fast'); // Mostra o fundo do menu com fade
+            $('#menu').addClass('rotateMenuBtn'); // Adiciona classe que rotaciona o botão
+            return 'slideOn'; // Aplica classe que desloca o menu para a direita, exibindo-o
         });
     }
 }
 
-// Aplicativo (camada de controle)
+// Construtor de eventos do App
 function runApp(){
-    console.log('meu app');
 
-    // Monitorando botão do menu principal
+    // Monitorando click/touch no botão do menu principal
     $(document).on('click', '#menu', toggleMenu);
 
-    // Monitorando o 'menuModal'
+    // Monitorando click/touch no 'menuModal'
     $(document).on('click', '#menuModal', toggleMenu);
 
 }
 
-
-
+// Tudo pronto? Vamos 'rodar' o App
 app.start();
